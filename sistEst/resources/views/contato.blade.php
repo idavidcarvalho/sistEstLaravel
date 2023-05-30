34364943b0926760c2c61095d91f400025c7e7bf
David <davidsoares585@gmail.com>
2023-05-30 09:43:56 -0300

    codigo do projeto

---
+++ sistEst/resources/views/contato.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.cdnfonts.com/css/porcelain-2" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/global.css">
+    <link rel="stylesheet" href="/css/home.css">
+    <script src="https://kit.fontawesome.com/3951763622.js" crossorigin="anonymous"></script>
+
+
+    <title>sistEst</title>
+</head>
+
+<body>
+    <header>
+        <div class="divLogo">
+            <p class="logo">
+                Sist<span>Est</span>
+            </p>
+        </div>
+        <nav class="menu">
+            <ul class="menuList">
+                <li class="menuItem">
+                    <a href="#" class="menuLink"><i class="fa-solid fa-car" style="color: #ffffff;"></i>
+                        Funcionalidades</a>
+                </li>
+                <li class="menuItem">
+                    <a href="#" class="menuLink"> <i class="fa-solid fa-dollar-sign" style="color: #ffffff;"></i> Planos
+                        e Preços</a>
+                </li>
+                <li class="menuItem">
+                    <a href="contato" class="menuLink"> <i class="fa-solid fa-envelope"
+                            style="color: #ffffff;"></i>Contato</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+    <main>
+       <h1>Passa o zap slsks</h1>
+    </main>
+    <footer>
+            <p>&copy; 2023 SistEst. Todos os direitos reservados.</p>
+    </footer>
+</body>
+
+</html>
---
+++ sistEst/resources/views/home.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.cdnfonts.com/css/porcelain-2" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="/css/home.css">
+    <script src="https://kit.fontawesome.com/3951763622.js" crossorigin="anonymous"></script>
+
+
+    <title>sistEst</title>
+</head>
+
+<body>
+    <header>
+        <div class="divLogo">
+            <p class="logo">
+                Sist<span>Est</span>
+            </p>
+        </div>
+        <nav class="menu">
+            <ul class="menuList">
+                <li class="menuItem">
+                    <a href="#" class="menuLink"><i class="fa-solid fa-car" style="color: #ffffff;"></i>
+                        Funcionalidades</a>
+                </li>
+                <li class="menuItem">
+                    <a href="#" class="menuLink"> <i class="fa-solid fa-dollar-sign" style="color: #ffffff;"></i> Planos
+                        e Preços</a>
+                </li>
+                <li class="menuItem">
+                    <a href="contato" class="menuLink"> <i class="fa-solid fa-envelope"
+                            style="color: #ffffff;"></i>Contato</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+    <main>
+        <section class="principal">
+            <div class="sobre">
+                <div class="divResumo">
+                    <h1 class="titulo">Sistema de Estacionamento (SistEst)</h1>
+                    <p class="resumo">
+                        Controlar seu estacionamento nunca foi tão fácil como agora! A ESTACIONE NET é o app para
+                        gerenciamento de estacionamento de pequeno e médio porte mais barato do Brasil. É o que o seu
+                        negócio precisa!
+                    </p>
+                </div>
+            </div>
+            <div class="divImgSisEst">
+                <img src="/images/ilustrationSistEst.jpg" alt="Imagem do SistEst" class="imgSistEst">
+            </div>
+        </section>
+
+        <section class="secundaria">
+            <h2 class="titulo">A gestão do seu estacionamento facilitado é com o SistEst</h2>
+            <div class="cards">
+                <div class="card">
+                    <h3 class="cardIcone" ><i class="fa-solid fa-cash-register"></i></h3>
+                    <h3 class="cardTitulo"> Controle de caixa</h3>
+                    <p class="cardTexto">
+                        Abertura e fechamento de caixa com relatórios. 
+                        Sistema simples integrado com seu aplicativo Android. 
+                        Com as opções de cobrança do estacionamento por preço ou por tempo.
+                    </p>
+                </div>
+                <div class="card">
+                    <h3 class="cardIcone" ><i class="fa-solid fa-camera"></i></h3>
+                    <h3 class="cardTitulo"> Captura da placa com a câmera</h3>
+                    <p class="cardTexto">
+                        Utilize a câmera do seu smartfone 
+                        para capturar a placa do carro. Com isso você deixa 
+                         seu negócio mais ágil e evitar erros de digitação.
+                    </p>
+                </div>
+                <div class="card">
+                    <h3 class="cardIcone" ><i class="fa-solid fa-mobile"></i></h3>
+                    <h3 class="cardTitulo"> Aplicativo Off-line</h3>
+                    <p class="cardTexto">
+                                    O aplicativo não necessita de internet 
+                        para funcionar. Melhorando o custo e eficiência 
+                        no estacionamento.
+                    </p>
+                </div>
+                <div class="card">
+                    <h3 class="cardIcone" ><i class="fa-solid fa-square-parking"></i></h3>
+                    <h3 class="cardTitulo"> Tipos de Estacionamentos</h3>
+                    <p class="cardTexto">
+                        Rotativo, Cobrança por tempo e Mensalistas
+                    </p>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+            <p>&copy; 2023 SistEst. Todos os direitos reservados.</p>
+    </footer>
+</body>
+
+</html>
